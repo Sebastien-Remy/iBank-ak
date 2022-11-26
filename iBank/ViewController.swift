1a457fa8e39b607c9be0bd76b03b030f23bf353a
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-26 09:11:52 +0100

    Initial Commit

---
+++ iBank/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iBank
+//
+//  Created by Sebastien REMY on 26/11/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
